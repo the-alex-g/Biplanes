d5a504ddab3bb24f85f0e8cfbde02361926b894b
the-alex-g <alex.gestwicki@gmail.com>
2022-12-10 07:21:48 -0500

    Add three-dimensional movement

--- project/Plane/Biplane.gd
+++ project/Plane/Biplane.gd
@@ -1,16 +1,15 @@
 extends KinematicBody
 
 # turning speed in revolutions per second
-export var turn_speed := 0.6
-export var flight_speed := 1.0
+export var turn_speed := 0.8
+export var flight_speed := 2.0
 
 
 func _physics_process(delta:float)->void:
-	rotation += Vector3(
-		Input.get_axis("up", "down"),
-		Input.get_axis("right", "left"),
-		Input.get_axis("roll_right", "roll_left")
-	) * turn_speed * delta
-	var direction := Vector3.FORWARD * flight_speed * delta
-	direction = direction.rotated(Vector3.UP, rotation.y)
-	var _collision := move_and_collide(direction)
+	rotate_object_local(Vector3.UP, Input.get_axis("right", "left") * turn_speed * delta)
+	rotate_object_local(Vector3.RIGHT, Input.get_axis("up", "down") * turn_speed * delta)
+	rotate_object_local(Vector3.FORWARD, Input.get_axis("roll_left", "roll_right") * turn_speed * delta)
+	
+	var movement_vector := transform.basis.xform(Vector3.FORWARD)
+	
+	var _collision := move_and_collide(movement_vector * flight_speed * delta)
